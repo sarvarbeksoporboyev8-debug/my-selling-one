3a7dbd4dfde9e9eb36a3870bc4cc414995e845b4
Ona <ona@gitpod.io>
2026-01-15 15:35:55 +0000

    Simplify domain layer - remove unused repositories/usecases
    - Remove repositories and usecases (not needed for MVP)
    - Rewrite ListingMapper with correct entity mappings
    - Add dw_domain import to discover_screen.dart
    - Keep only entities and mappers
    
    Co-authored-by: Ona <no-reply@ona.com>

--- apps/dont_waste_flutter/packages/dw_domain/lib/dw_domain.dart
+++ apps/dont_waste_flutter/packages/dw_domain/lib/dw_domain.dart
@@ -10,17 +10,5 @@ export 'entities/user.dart';
 export 'entities/location.dart';
 export 'entities/filters.dart';
 
-// Repositories
-export 'repositories/listings_repository.dart';
-export 'repositories/reservations_repository.dart';
-export 'repositories/offers_repository.dart';
-export 'repositories/watches_repository.dart';
-export 'repositories/auth_repository.dart';
-
-// Usecases
-export 'usecases/search_listings.dart';
-export 'usecases/reserve_listing.dart';
-export 'usecases/manage_watches.dart';
-
 // Mappers
 export 'mappers/listing_mapper.dart';
